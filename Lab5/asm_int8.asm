c394b1dc8b0e309058fa4a79184bc76a59b157d3
floralunit <anya71034@gmail.com>
2023-12-02 01:25:15 +0300

    lab5

--- Lab5/asm_int8.asm
+++ Lab5/asm_int8.asm
@@ -1,36 +1,44 @@
 section .data
-extern a
-extern b
-extern c
-extern numerator_asm
-extern denominator_asm
-extern result_asm
-extern remainder_asm
-
-; y = (-3*a-b+53)/(c-a/2+1)
+    a dd 0.0
+    b dd 0.0
+    c dd 0
+    numerator dd 0.0
+    denominator dd 0.0
+    result dd 0.0
 
 section .text
-global asmfunc
-asmfunc:
-	movsx eax, byte [a]
-    	imul eax, -3         ; Умножение EAX на -3
-    	movsx ebx, byte [b]          ; Вычитание b из EAX
-    	sub eax, ebx
-    	add eax, 53          ; Добавление 53 к EAX
-    	mov [numerator_asm], eax
-    	
-    	movsx ebx, byte [a]    ; Загрузка значения a в AX
-    	sar ebx, 1           ; Половинное деление значения a, т.е. побитовый сдвиг
-    	movsx ecx, byte [c]          ; Вычитание a/2 из с
-    	sub ecx, ebx
-    	inc ecx
-    	mov [denominator_asm], ecx
-    	
-    	mov eax, dword [numerator_asm]
-    	cdq
-    	idiv dword [denominator_asm]
-    	mov dword [result_asm], eax
-    	mov dword [remainder_asm], edx
+    global _start
 
-	ret
+_start:
+    ; ввод исходных данных с клавиатуры
+    ; вывод диапазона возможных значений
+    ; ...
 
+    ; вычисление числителя (-3*a-b+53)
+    fld dword [a]          ; загрузка переменной a в стек сопроцессора
+    fmul dword -3.0        ; умножение на -3
+    fld dword [b]          ; загрузка переменной b в стек сопроцессора
+    fsub                    ; вычитание b из -3*a
+    fld dword 53.0          ; загрузка числа 53 в стек сопроцессора
+    fadd                    ; сложение с -3*a-b
+    fstp dword [numerator]  ; сохранение результата в переменную numerator
+
+    ; вычисление знаменателя (c-a/2+1)
+    fld dword [c]          ; загрузка переменной c в стек сопроцессора
+    fld dword [a]          ; загрузка переменной a в стек сопроцессора
+    fdiv dword 2.0         ; деление a на 2
+    fsub                    ; вычитание a/2 из c
+    fld dword 1.0           ; загрузка числа 1 в стек сопроцессора
+    fadd                    ; сложение с c-a/2
+    fstp dword [denominator] ; сохранение результата в переменную denominator
+
+    ; вычисление результата числитель/знаменатель
+    fld dword [numerator]     ; загрузка числителя в стек сопроцессора
+    fld dword [denominator]   ; загрузка знаменателя в стек сопроцессора
+    fdiv                        ; деление числителя на знаменатель
+    fstp dword [result]         ; сохранение результата в переменную result
+
+    ; вывод результатов вычислений на экран
+    ; ...
+
+    ; завершение программы
